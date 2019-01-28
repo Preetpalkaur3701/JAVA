ddf4ed6dce2abfab7c784e60199f52f2e103abf7
Preetpal Kaur <preetpalok123@gmail.com>
2019-01-28 12:19:29 +0530

    Expression1 in java

---
+++ one.java
@@ -0,0 +1,10 @@
+class one
+{
+
+     public static void main(String []args){
+        System.out.println("* * * *");
+        System.out.println("* * * *");
+        System.out.println("* * * *");
+        System.out.println("* * * *");
+     }
+}
