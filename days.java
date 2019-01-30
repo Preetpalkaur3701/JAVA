e9742643e17a4995ee4982f92d336ce6a4b34bae
Preetpal Kaur <preetpalok123@gmail.com>
2019-01-30 22:50:22 +0530

    days in week

---
+++ days.java
@@ -0,0 +1,49 @@
+class days
+{
+	public static void main(string args[])
+	{
+		int a;
+		System.out.Print("Enter the number");
+		a = Integer.Parse.Int(args[0]);
+
+		if (a=1);{
+			result= Monday;
+			System.out.Print(result);
+		}
+
+		else if(a=2);{
+			result= Tuesday;
+			System.out.Print(result);
+		}
+
+		else if(a=3);{
+			result= Wednesday;
+			System.out.Print(result);
+		}
+
+		else if(a=4);{
+			result= Thursday;
+			System.out.Print(result);
+		}
+
+		else if(a=5);{
+			result= Friday;
+			System.out.Print(result);
+		}
+
+		else if(a=2);{
+			result= Saturday;
+			System.out.Print(result);
+		}
+
+		else if(a=2);{
+			result= Sunday;
+			System.out.Print(result);
+		}
+
+		else{
+			result=  Wrong Input;
+			System.out.Print(result);
+		}
+	}
+}
