0d1f571edb0caf21334fae2f713429036ada61aa
Priyanka788 <51821861+Priyanka788@users.noreply.github.com>
2019-10-10 17:54:34 +0530

    Print pattern of opposite of Right angle triangle using java coding..
    ****
    ***
    **
    *

---
+++ Pat1.java
@@ -0,0 +1,17 @@
+public class Pat1
+{
+public static void main(String [] args)
+{
+for(int i=1;i<=5;i++)
+	
+{
+	
+  for(int j=5;j>=i;j--)
+  { 
+                    System.out.print(" * ");
+	}    
+                System.out.print("\n");
+				
+}
+}
+}				
