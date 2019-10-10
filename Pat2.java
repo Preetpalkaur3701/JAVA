e8df02050411bf63ac62a1b2098fa936e9cb6019
Priyanka788 <51821861+Priyanka788@users.noreply.github.com>
2019-10-10 17:55:50 +0530

    Print pattern of Triangle or pyramid using java coding..
    * * * *
     * * *
      *  *
        *

---
+++ Pat2.java
@@ -0,0 +1,18 @@
+public class Pat2
+{
+public static void main(String [] args)
+{
+for(int i=1;i<=5;i++)
+{
+  for(int j=5;j>=i;j--)
+    { 
+                    System.out.print(" ");
+	}
+for(int k=1;k<=i;k++)
+{ 
+     System.out.print(" * ");
+}
+ System.out.print("\n");
+}}
+} 
+	
